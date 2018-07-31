dab7dfd61b8becb931a5dbc20d71775b7117d8b2
Imhotep Wallace <imalwallace@yahoo.com>
2018-07-31 16:41:38 -0400

    Added Hello World java file
    did the thing

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.revature.hello; //package declaration goes on the first line
+//package, just a name space, corresponds to a directory
+/*
+	classes should be pascal case (FirstSecondThird)
+	one public class per file, same name as file
+*/
+public class Helloworld {
+	//MAIN METHOD
+	//point of entry into Java program
+	//method should be camelCase (firstSecondThird)
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+	
+}
